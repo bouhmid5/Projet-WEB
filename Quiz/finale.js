074ea728c51bb511589a29e35c26822bd77c1a35
bouhmid5 <132016896+bouhmid5@users.noreply.github.com>
2024-12-07 20:19:57 +0100

    Add files via upload

---
+++ Quiz/finale.js
@@ -0,0 +1,9 @@
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener("keyup", () =>{
+    saveScoreBtn.disabled = !username.value;
+});
